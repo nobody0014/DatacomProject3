9ccfd17432c717a2afb01558899a79268cc3cbf5
Joakim Pedersen Nilfjord <joakim.nilfjord@gmail.com>
2016-11-24 22:16:33 +0700

    Adding services

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.io.InterruptedIOException;
+
 /**
  * Created by Wit on 11/22/2016 AD.
  */
@@ -5,7 +8,12 @@
 //File where everything start, but we probably wont use it till the project is done or for testing our classes
 //Comment stuff out before writing your crap and make sure u dont push and pull anyhow
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
+        Service service = new Service();
+        service.start();
+        Thread.sleep(2000);
+        service.unregister();
+
 
     }
 }
--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,7 +1,38 @@
-/**
- * Created by Wit on 11/22/2016 AD.
- */
+import java.net.InetAddress;
+import java.util.Set;
+import javax.jmdns.*;
+
 
 //Service discoverer
 public class Service {
+    ServiceDiscovery server;
+    JmDNS jmdns;
+    ServiceInfo serviceInfo;
+
+    public Service(){
+        try{
+            server = new ServiceDiscovery();
+            serviceInfo = ServiceInfo.create("_http._tcp.local.", "Heyooo", 80, "path=index.html");
+            jmdns = JmDNS.create(InetAddress.getLocalHost());
+        }catch (Exception e){System.out.println(e.getMessage());}
+
+    }
+    public void start(){
+        server.run();
+        register();
+    }
+    public void register(){
+        try{
+            ServiceInfo serviceInfo = ServiceInfo.create("_http._tcp.local.", "Heyooo", 80, "path=index.html");
+            jmdns.registerService(serviceInfo);
+        }catch (Exception e){System.out.println(e.getMessage());}
+
+    }
+    public void unregister(){
+        jmdns.unregisterAllServices();
+    }
+
+    public Set<String> getIPs(){
+        return server.getIPs();
+    }
 }
