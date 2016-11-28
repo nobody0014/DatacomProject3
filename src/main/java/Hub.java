25be5617107fc37108db2bd864beb784a72b579b
Phornphawit Manasut <wittz0014@gmail.com>
2016-11-28 19:47:53 +0700

    Fix hub, implment Main later, yolo swag

--- src/main/java/Hub.java
+++ src/main/java/Hub.java
@@ -1,7 +1,10 @@
 /**
  * Created by Wit on 11/22/2016 AD.
  */
+import java.lang.reflect.Array;
 import java.net.URI;
+
+import com.sun.media.jfxmedia.track.Track;
 import com.turn.ttorrent.common.Torrent;
 import com.turn.ttorrent.client.*;
 import com.turn.ttorrent.tracker.TrackedPeer;
@@ -20,7 +23,6 @@ import java.util.*;
 
 //Client used for downloading, right now using ttorent protocol
 public class Hub {
-    public Hub(){}
 
     //Downloading the file from people
     //Downloading steps
@@ -29,117 +31,89 @@ public class Hub {
     //3) start the client download
     public void download(String fileName){
         try{
-            startClient(false,0.0,0.0,fileName,".",null);
+            startClient(Inet4Address.getLocalHost(), new File(fileName), new File("."));
+
         }catch (Exception e){e.printStackTrace();}
 
     }
 
-    //Uploading the file to people
-    //Uploading steps
-    //1) create torrent of the file
-    //2) create tracker server of the file
-    //3) sends the tracker server of the file to people
-    //4) start the damn client
+
     public void upload(String fileName){
         try{
-            upload(fileName,Inet4Address.getLocalHost().getHostAddress());
+            File fn = new File(fileName + ".torrent");
+            File out = new File(".");
+            List<InetAddress> ips = getIPs();
+            List<Tracker> trackers = new ArrayList<>();
+            List<List<URI>> uris = new ArrayList<>();
+            uris.add(new ArrayList<>());
+            for (InetAddress ip : ips){
+                uris.get(0).add(new URI("http://" + ip.getHostAddress() + ":6969/announce"));
+                trackers.add(generateTrackerServer(ip));
+            }
+            generateTorrentFile(fn,uris,trackers);
+            for (InetAddress ip: ips){
+                startClient(ip,fn,out);
+            }
+
         }catch (Exception e){e.printStackTrace();}
     }
-    public void upload(String fileName, String url){
-        try{
-            List<String> announceURLs  = new ArrayList<>();
-            announceURLs.add(url);
-            generateTorrentFile(".", fileName, announceURLs);
-            generateTrackerServer(6969,".");
-            startClient(true,0.0,0.0,fileName,".",null);
-        }catch (Exception e){e.printStackTrace();}
-    }
 
-    //isMaster is telling if you are the downloader
-    //ifacevalue --> own ipaddress
-    //outputValue is the directory --> "." as default
-    //maxupload/maxdownload can be set
-    //filename --> is the downloading file name
-    public void startClient(boolean isMaster, double maxDownloadRate, double maxUploadRate,
-                            String fileName, String outputValue, String ifaceValue){
-        try {
-            Client c = new Client(
-                    getIPv4Address(ifaceValue),
-                    SharedTorrent.fromFile(
-                            new File(fileName + ".torrent"),
-                            new File(outputValue)));
-            c.setMaxDownloadRate(maxDownloadRate);
-            c.setMaxUploadRate(maxUploadRate);
-            if(isMaster){c.share();}
-            else{c.download();}
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    public List<InetAddress> getIPs() throws IOException{
 
-    //src is the folder --> "."
-    //fileName is the original file name
-    //announceURL would require the damn IP of the tracker file, default port is 6969
-    public void generateTorrentFile(String src, String fileName, List<String> announceURLs)
-            throws InterruptedException, IOException, NoSuchAlgorithmException {
-        OutputStream fos = null;
-        try{
-            fos = new FileOutputStream(fileName);
-            List<URI> announceURIs = new ArrayList<URI>();
-            for (String url : announceURLs) {
-                announceURIs.add(new URI(url));
-            }
-            List<List<URI>> announceList = new ArrayList<List<URI>>();
-            announceList.add(announceURIs);
-            File source = new File(src);
-            String creator = String.format("%s (ttorrent)", System.getProperty("user.name"));
-            Torrent torrent;
-            if (source.isDirectory()) {
-                List<File> files = new
-                        ArrayList<>(FileUtils.listFiles(source, TrueFileFilter.TRUE, TrueFileFilter.TRUE));
-                Collections.sort(files);
-                torrent = Torrent.create(source,files,announceList,creator);
-            } else {
-                torrent = Torrent.create(source,announceList,creator);
-            }
-            torrent.save(fos);
-        }catch(Exception e){e.printStackTrace();}
-        finally {if(fos != System.out){IOUtils.closeQuietly(fos);}}
-    }
-    public void generateTrackerServer(int port, String directory){
-        FilenameFilter filter =  (dir, name) -> {return name.endsWith(".torrent");};
-        try {
-            Tracker t = new Tracker(new InetSocketAddress(port));
-            File parent = new File(directory);
-            for (File f : parent.listFiles(filter)) {
-                t.announce(TrackedTorrent.load(f));
-            }
-
-            t.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(2);
-        }
-    }
-    private static Inet4Address getIPv4Address(String iface)
-            throws SocketException, UnsupportedAddressTypeException, UnknownHostException {
-        if (iface != null) {
-            Enumeration<InetAddress> addresses =
-                    NetworkInterface.getByName(iface).getInetAddresses();
-            while (addresses.hasMoreElements()) {
-                InetAddress addr = addresses.nextElement();
-                if (addr instanceof Inet4Address) {
-                    return (Inet4Address)addr;
+        List<InetAddress> ips = new ArrayList<>();
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets)){
+            Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+            if(inetAddresses.hasMoreElements()){
+                for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+                    ips.add(inetAddress);
                 }
             }
         }
 
-        InetAddress localhost = InetAddress.getLocalHost();
-        if (localhost instanceof Inet4Address) {
-            return (Inet4Address)localhost;
+        return ips;
+    }
+
+
+    public void startClient(InetAddress ip, File fileName, File outputValue){
+
+        try {
+            Client c = new Client(ip, SharedTorrent.fromFile(fileName, outputValue));
+            c.setMaxDownloadRate(0.0);
+            c.setMaxUploadRate(0.0);
+            c.share();
+            c.waitForCompletion();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        throw new UnsupportedAddressTypeException();
     }
+
+
+
+    public void generateTorrentFile(File fileName, List<List<URI>> uris, List<Tracker> trackers)
+            throws InterruptedException, IOException, NoSuchAlgorithmException {
+
+        Torrent t = Torrent.create(fileName, uris , "Wit/Jo");
+        File f = new File(fileName + ".torrent");
+        t.save(new FileOutputStream(f));
+        for (Tracker tracker : trackers){
+            tracker.announce(TrackedTorrent.load(f));
+        }
+
+
+
+    }
+
+    public Tracker generateTrackerServer(InetAddress iddr) throws IOException{
+
+
+        Tracker t = new Tracker(iddr);
+        t.start();
+        return t;
+
+
+    }
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.Inet4Address;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.*;
 import java.io.*;
 import java.util.concurrent.*;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.client.HttpClients;
@@ -14,6 +17,8 @@ import spark.Route;
 import static spark.Spark.*;
 import org.apache.http.*;
 
+import javax.print.URIException;
+
 //File where everything start, but we probably wont use it till the project is done or for testing our classes
 //Comment stuff out before writing your crap and make sure u dont push and pull anyhow
 public class Main {
@@ -21,53 +26,41 @@ public class Main {
         public static final int DEFAULT_PORT = 19999;
         public static final int TORRENT_DEFAULT_PORT = 29999;
     }
+    static enum Status{WAITING, WORKING}
+    static Status ClientStatus;
 
     public static void main(String[] args){
+        ClientStatus = Status.WAITING;
         Service s = new Service("_http._tcp.local.");
-        Hub h = new Hub();
         s.start();
         //s.registered()
         port(Config.DEFAULT_PORT);
 
         //Route
-        post("/download", download(h));
-        post("/upload", upload(h,s));
+        post("/download", download());
+        post("/upload", upload(s));
     }
-    private static Route download(Hub h) {
+    private static Route download() {
         return (request, response) -> {
-            String host = request.host();
-            int port = request.port();
-            String fileName = request.params("FileName");
             String toReturn = null;
-            System.out.println("Received download command from: " + host);
-            System.out.println("Will attempt to download the torrrent file: " + fileName);
-            TorrentClient tc = new TorrentClient(port,host,fileName);
-            tc.download();
-            System.out.println("Finish downloading the torrent file, connecting to the server");
-            h.download(fileName);
+            if(ClientStatus == Status.WAITING){
+
+            }
             return toReturn;
         };
     }
 
-    private static Route upload(Hub h, Service s) {
+    private static Route upload(Service s) {
         return (request, response) -> {
-            System.out.println("Received upload command");
-            String fileName = request.params("fileName");
             String toReturn = null;
-            Notifier n = new Notifier(fileName,s.getIPs(),Config.DEFAULT_PORT);
-            TorrentServer ts = new TorrentServer(Config.TORRENT_DEFAULT_PORT,fileName + ".torrent");
-            if ( fileName != null && fileName.length() > 0) {
-                h.upload(fileName);
-                ts.run();
-                //Sleep so that the torrent server has time to set itself up
-                Thread.sleep(2000);
-                n.run();
-            }
-            else {
-                toReturn = "Invalid fileName";
-                halt(400, toReturn);
+            if(ClientStatus == Status.WAITING){
+
+
+
+
             }
             return toReturn;
         };
     }
+
 }
